4afa08461d3667280b2f76ce2b8c15696be5ebf9
Jisoo Kim <jisoojamie@gmail.com>
2023-10-24 18:41:25 +0900

    feat: 전체 index 페이지 추가

--- countdownTimer/index.js
+++ countdownTimer/index.js
@@ -7,12 +7,12 @@ const stop = document.querySelector('.stop');
 const reset = document.querySelector('.reset');
 
 // 타이머 동작 로직
-let time = 0;
+let time = 3600;
 let clear; // setInterval 함수로 생성된 인터벌 ID를 저장하는 변수, clearInterval에 전달.
 
 const startButtonHandler = () => {
-  timerStart();
-  clear = setInterval(timerStart, 1000);
+  countdownStart();
+  clear = setInterval(countdownStart, 1000);
 };
 
 const stopButtonHandler = () => {
@@ -20,19 +20,24 @@ const stopButtonHandler = () => {
 };
 
 const resetButtonHandler = () => {
-  timer.innerHTML = '00 : 00 : 00';
+  timer.innerHTML = '01 : 00 : 00';
+  time = 3600;
 };
 
-const timerStart = () => {
-  const hours = Math.floor(time / 3600);
-  const checkMinutes = Math.floor(time / 60);
-  const minutes = checkMinutes % 60;
-  const seconds = time % 60;
+const countdownStart = () => {
+  if (time >= 0) {
+    const hours = Math.floor(time / 3600);
+    const checkMinutes = Math.floor(time / 60);
+    const minutes = checkMinutes % 60;
+    const seconds = time % 60;
 
-  timer.innerHTML = `${hours < 10 ? `0${hours}` : hours} : ${
-    minutes < 10 ? `0${minutes}` : minutes
-  } : ${seconds < 10 ? `0${seconds}` : seconds}`;
-  time++;
+    timer.innerHTML = `${hours < 10 ? `0${hours}` : hours} : ${
+      minutes < 10 ? `0${minutes}` : minutes
+    } : ${seconds < 10 ? `0${seconds}` : seconds}`;
+    time--;
+  } else {
+    clearInterval(clear);
+  }
 };
 
 // 버튼에 이벤트 추가
