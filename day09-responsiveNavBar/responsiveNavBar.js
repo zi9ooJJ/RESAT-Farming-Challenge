dd659dd393a320e6432920d6bbdb8515fae70b43
Jisoo Kim <jisoojamie@gmail.com>
2023-11-02 17:02:50 +0900

    style:반응형 내비게이션 바 스타일 적용

--- day09-responsiveNavBar/responsiveNavBar.js
+++ day09-responsiveNavBar/responsiveNavBar.js
@@ -1,19 +1,15 @@
 const navBurgerBtn = document.querySelector('.nav-burger-icon');
 const navXBtn = document.querySelector('.nav-x-icon');
 const navListWrapperMobile = document.querySelector('.nav-list-wrapper-mobile');
-const headerNavMobile = document.querySelector('.header-nav-mobile');
 
 navBurgerBtn.addEventListener('click', () => {
   navListWrapperMobile.classList.toggle('active');
-  headerNavMobile.classList.toggle('active');
 });
 
 navXBtn.addEventListener('click', () => {
   navListWrapperMobile.classList.remove('active');
-  headerNavMobile.classList.remove('active');
 });
 
-// headerNavMobile.addEventListener('click', () => {
-//   navListWrapperMobile.classList.remove('active');
-//   headerNavMobile.classList.remove('active');
-// });
+navListWrapperMobile.addEventListener('click', () => {
+  navListWrapperMobile.classList.remove('active');
+});
